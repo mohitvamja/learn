e17f0db6915b64a7addd98f7b4c9cc4d38976625
mohitvamja <mohitvamja1991@gmail.com>
2022-02-15 17:37:58 +0530

    add a new file

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     config: {
         mixins: {
-            'Magento_Checkout/js/action/set-shipping-information': {
+            'Magento_Checkout/js/action/set-shipping-information1': {
                 'Sale_Paymenttext/js/order/set-shipping-information-mixin': true
             }
         }
